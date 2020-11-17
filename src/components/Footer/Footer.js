3315e3d69b04991e5f4de617f48bc6f63c36fbef
Ingrid Otani <inaotani@gmail.com>
2020-11-17 20:14:35 -0300

    development of footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from './style.css'
+
+const Footer = () => { 
+    return (
+        <footer id="footer">
+            <p>Copyright © 2020 Pet Shop</p>
+        </footer>
+    )
+}
+
+export default Footer
