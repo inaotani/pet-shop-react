7afa883da2012ee0c5d1f3cb0dec32dafc7e0869
Ingrid Otani <inaotani@gmail.com>
2020-12-09 19:40:29 -0300

    adjust on header on admin

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,26 @@
-import React from "react";
-import Style from "./style.css";
+import React, { useContext } from "react";
 import Header from "./../Header";
 import Footer from "./../Footer";
+import { store } from "../../store";
 
 const Layout = ({ children }) => {
+  const globalLogin = useContext(store);
+  const { login } = globalLogin;
+  const menuUser = [
+    { link: "/", text: "Página Inicial" },
+    { link: "/racoes", text: "Rações" },
+    { link: "/minha-conta", text: "Minha Conta" },
+  ];
+
+  const menuAdmin = [
+    { link: "/", text: "Página Inicial" },
+    { link: "/racoes", text: "Rações" },
+    { link: "/minha-conta", text: "Minha Conta" },
+    { link: "/estoque", text: "Estoque" },
+  ];
   return (
     <>
-      <Header
-        menuList={[
-          { link: "/", text: "Página Inicial" },
-          { link: "/racoes", text: "Rações" },
-          { link: "/minha-conta", text: "Minha Conta" },
-        ]}
-      />
+      <Header menuList={login.admin ? menuAdmin : menuUser} />
 
       <main id="target">{children}</main>
       <Footer />
